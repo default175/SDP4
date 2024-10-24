698ccae02e9b6e3cb010d4a72c6db0c091e319b5
Ulan Bakytbekov <153665350+default175@users.noreply.github.com>
2024-10-24 12:33:19 +0500

    Add files via upload

---
+++ untitled10/src/IteratorDemo.java
@@ -0,0 +1,11 @@
+public class IteratorDemo {
+    public static void main(String[] args) {
+        NameRepository nameRepository = new NameRepository();
+        Iterator iterator = nameRepository.getIterator();
+
+        System.out.println("Iterating through names:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
